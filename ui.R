a2f511f699ec76bd44c59cd2ece79b92e34bbb4d
aloknayak29 <aloknayak29@gmail.com>
2013-10-30 13:46:03 +0530

    Create ui.R
    first

---
+++ ui.R
@@ -0,0 +1,30 @@
+library(shiny)
+
+shinyUI(pageWithSidebar(
+  headerPanel("Uploading Files"),
+  sidebarPanel(
+    fileInput('file1', 'Choose CSV File',
+              accept=c('text/csv', 'text/comma-separated-values,text/plain')),
+    tags$hr(),
+    checkboxInput('header', 'Header', TRUE),
+    radioButtons('sep', 'Separator',
+                 c(Comma=',',
+                   Semicolon=';',
+                   Tab='\t'),
+                 'Comma'),
+    radioButtons('quote', 'Quote',
+                 c(None='',
+                   'Double Quote'='"',
+                   'Single Quote'="'"),
+                 'Double Quote'),
+    checkboxGroupInput("xvars", "Choose x variables", as.character(1:10)),
+    selectInput("yvar", "Choose y variable:", 
+               choices = as.character(c(1:10))),
+    #checkboxInput("", "Show LM output"),
+    submitButton("Update View")
+  ),
+  mainPanel(
+    verbatimTextOutput('lm_output'),
+    tableOutput('contents')
+  )
+))
