4d0fb4fcfe96b3469708010725739a06af87a8a9
aloknayak29 <aloknayak29@gmail.com>
2013-11-17 18:51:01 +0530

    Update server.R
    as per shinyapp alpha

--- server.R
+++ server.R
@@ -16,33 +16,26 @@ shinyServer(function(input, output) {
     # file, it will be a data frame with 'name', 'size', 'type', and 'datapath' 
     # columns. The 'datapath' column will contain the local filenames where the 
     # data can be found.
-    df()
+    head(df(),10)
   })
+  output$cnames <- renderPrint({
+    names(df())
+  })
+  
   output$lm_output <- renderPrint({
-    #print('asdfsaf')
-    print(input$xvars)
-    #print(dbool)
+    t1 <- input$xvars
+    t2 <- input$yvar
     if(dbool == TRUE) {
       x <- df()
       xnamesall <- names(x)
-      xnames <- xnamesall[as.integer(input$xvars)]
-      print(xnames)
-      print(input$yvar)
-      yvariable <- xnamesall[as.integer(input$yvar)]
-      
-      print(yvariable)
+      xnames <- xnamesall[as.integer(t1)]
+      yvariable <- xnamesall[as.integer(t2)]
       ytext <- paste(yvariable,"~")
       fmla <- as.formula(paste(ytext, paste(xnames, collapse= "+")))
-      #print(fmla)
       fit <- lm(fmla, x, na.action=na.omit)
       print(summary(fit))
     }
     else
-      print('ANALYSIS WILL APPEAR HERE')
-    #x <- df()
-    #xnames <- names(x)
-    #xnames <- xnames[input$xvars]
-    #yvariable <- xnames[input$yvar]
-    #fmla <- as.formula(paste(paste(yvariable," ~ ",sep=''), paste(xnames, collapse= "+")))
+      print('LINEAR REGRESSION ANALYSIS WILL APPEAR HERE')
   })
 })
